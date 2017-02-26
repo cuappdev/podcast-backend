cf95b4e23d3d266cfa468b72a07ac65342115c1a
Jma353 <jma353@cornell.edu>
2017-02-26 14:09:30 -0500

    Initial commit

---
+++ src/main/java/podcast/Application.java
@@ -0,0 +1,14 @@
+package podcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  /* App entry-point */
+  public static void main (String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
