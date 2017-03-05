bf5e2145b8d15abc9e78aa9db36f780922e58057
Jma353 <jma353@cornell.edu>
2017-03-05 14:12:21 -0500

    Fix imports / spacing

--- src/main/java/podcast/models/entities/FollowersFollowings.java
+++ src/main/java/podcast/models/entities/FollowersFollowings.java
@@ -3,46 +3,43 @@ package podcast.models.entities;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.UUID;
+import com.couchbase.client.java.document.json.JsonObject;
 import com.fasterxml.uuid.Generators;
 import lombok.Getter;
 
 public class FollowersFollowings extends Entity {
-    /** Represents a follower-following relationship owned by a user.
-        Note that followers and followings may not contain duplicates. **/
-    @Getter private UUID uuid;
-    @Getter private List<UUID> followers;
-    @Getter private List<UUID> followings;
 
-    public FollowersFollowings() {
-        this.uuid = Generators.timeBasedGenerator().generate();
-        this.followers = new ArrayList<UUID>();
-        this.followings = new ArrayList<UUID>();
-    }
+  /** Represents a follower-following relationship owned by a user.
+   Note that followers and followings may not contain duplicates. **/
+  @Getter private UUID uuid;
+  @Getter private List<UUID> followers;
+  @Getter private List<UUID> followings;
 
-    public boolean addFollower(UUID followerUUID) {
-        if(!(this.followers.contains(followerUUID))) {
-            return this.followers.add(followerUUID);
-        }
-        return false;
-    }
+  public FollowersFollowings() {
+    this.uuid = Generators.timeBasedGenerator().generate();
+    this.followers = new ArrayList<UUID>();
+    this.followings = new ArrayList<UUID>();
+  }
 
-    public boolean removeFollower(UUID followerUUID) {
-        return this.followers.remove(followerUUID);
-    }
+  public boolean addFollower(UUID followerUUID) {
+    return !(this.followers.contains(followerUUID)) && this.followers.add(followerUUID);
+  }
 
-    public boolean addFollowing(UUID followingUUID) {
-        if(!(this.followers.contains(followingUUID))) {
-            return this.followings.add(followingUUID);
-        }
-        return false;
-    }
+  public boolean removeFollower(UUID followerUUID) {
+    return this.followers.remove(followerUUID);
+  }
 
-    public boolean removeFollowing(UUID followingUUID) {
-        return this.followers.remove(followingUUID);
-    }
+  public boolean addFollowing(UUID followingUUID) {
+    return !(this.followers.contains(followingUUID)) && this.followings.add(followingUUID);
+  }
 
-    public JsonObject toJsonObject() {
-        // TODO
-        return null;
-    }
+  public boolean removeFollowing(UUID followingUUID) {
+    return this.followers.remove(followingUUID);
+  }
+
+  public JsonObject toJsonObject() {
+    // TODO
+    return null;
+  }
+
 }
