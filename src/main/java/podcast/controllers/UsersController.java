1a20bd4177c452f278da3a1316386f3bd3626a9d
Jma353 <jma353@cornell.edu>
2017-03-05 13:05:01 -0500

    Google sign in endpoint (for now)

--- src/main/java/podcast/controllers/UsersController.java
+++ src/main/java/podcast/controllers/UsersController.java
@@ -1,10 +1,29 @@
 package podcast.controllers;
 
+import org.codehaus.jackson.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import podcast.services.GoogleService;
 
 @RestController
+@RequestMapping("/api/users")
 public class UsersController {
 
-  // TODO
+  @Autowired
+  private GoogleService googleService;
+
+  /**
+   * Google Sign In Endpoint
+   * @param idToken - Google ID
+   * @return - TODO
+   */
+  @RequestMapping(method = RequestMethod.POST, value = "/google_sign_in")
+  public JsonNode googleSignIn(@RequestParam(value="id_token", required = true) String idToken) {
+    // TODO - deal with actual user logistics here
+    return googleService.googleAuthentication(idToken);
+  }
 
 }
