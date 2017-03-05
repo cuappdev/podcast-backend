8dcc0b8a873bf4c3d85269ae9ad7c34b1cdc43bc
Jma353 <jma353@cornell.edu>
2017-03-05 12:57:14 -0500

    GoogleService for auth

--- src/main/java/podcast/services/GoogleService.java
+++ src/main/java/podcast/services/GoogleService.java
@@ -1,6 +1,21 @@
 package podcast.services;
 
+import org.codehaus.jackson.JsonNode;
+import org.springframework.stereotype.Service;
+import podcast.utils.HTTP;
+
+@Service
 public class GoogleService {
 
+  /**
+   * Google authentication request according to
+   * provided idToken retrieved on mobile client
+   * @param idToken - String idToken
+   * @return - JsonNode (response from Google)
+   */
+  public JsonNode googleAuthentication(String idToken) {
+    String uri = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + idToken;
+    return HTTP.get(uri);
+  }
 
 }
