8db7be26657ed9d4913c65077b148b3b54b821e8
Jma353 <jma353@cornell.edu>
2017-03-08 14:20:32 -0500

    Start adding fields to constant file

--- src/main/java/podcast/models/utils/Constants.java
+++ src/main/java/podcast/models/utils/Constants.java
@@ -1,7 +1,33 @@
 package podcast.models.utils;
 
 
+import podcast.models.entities.User;
+
 public class Constants {
+
+  /* RESOURCE NAMES */
+  public static final String USER = "user";
+  public static final String SESSION = "session";
+  public static final String SERIES = "series";
+  public static final String EPISODE = "episode";
+  public static final String FOLLOWER = "follower";
+  public static final String FOLLOWING = "following";
+  public static final String RELEASE = "release";
+  public static final String RECOMMENDATION = "recommendation";
+
+  /* FIELD NAMES */
+  public static final String TYPE = "type";
+  public static final String ID = "id";
+  public static final String GOOGLE_ID = "googleId";
+  public static final String EMAIL = "email";
+  public static final String FIRST_NAME = "firstName";
+  public static final String LAST_NAME = "lastName";
+  public static final String IMAGE_URL = "imageUrl";
+  public static final String NUMBER_FOLLOWERS = "numberFollowers";
+  public static final String NUMBER_FOLLOWINGS = "numberFollowing";
+  // TODO - more
+
+
 
   /* Secret encryption key */
   // public static final String SECRET_KEY = System.getenv("SECRET_KEY");
@@ -14,8 +40,8 @@ public class Constants {
   public static enum Type {
     USER,
     SESSION,
-    EPISODE,
     SERIES,
+    EPISODE,
     FOLLOWER,
     FOLLOWING,
     RELEASE,
@@ -25,17 +51,18 @@ public class Constants {
     @Override
     public String toString() {
       switch (this) {
-        case USER: return "user";
-        case SESSION: return "session";
-        case EPISODE: return "episode";
-        case SERIES: return "series";
-        case FOLLOWER: return "follower";
-        case FOLLOWING: return "following";
-        case RELEASE: return "release";
-        case RECOMMENDATION: return "recommendation";
+        case USER: return Constants.USER;
+        case SESSION: return Constants.SESSION;
+        case EPISODE: return Constants.EPISODE;
+        case SERIES: return Constants.SERIES;
+        case FOLLOWER: return Constants.FOLLOWER;
+        case FOLLOWING: return Constants.FOLLOWING;
+        case RELEASE: return Constants.RELEASE;
+        case RECOMMENDATION: return Constants.RECOMMENDATION;
         default: throw new IllegalArgumentException();
       }
     }
   }
+  
 
 }
