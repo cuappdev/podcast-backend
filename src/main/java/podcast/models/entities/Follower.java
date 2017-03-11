de0a6718a56266e5d18356aabd9f53a227426a11
Amit Mizrahi <amit@amizrahi.com>
2017-03-11 13:27:19 -0500

    Follower schema

--- src/main/java/podcast/models/entities/Follower.java
+++ src/main/java/podcast/models/entities/Follower.java
@@ -11,11 +11,11 @@ public class Follower extends Entity {
 
   @Getter private Constants.Type type = Constants.Type.FOLLOWER;
   @Getter private String ownerId;
-  @Getter private String followerId;
-  @Getter private String followerFirstName;
-  @Getter private String followerLastName;
-  @Getter private String followerUsername;
-  @Getter private String followerImageUrl;
+  @Getter private String id;
+  @Getter private String firstName;
+  @Getter private String lastName;
+  @Getter private String username;
+  @Getter private String imageUrl;
 
 
   /**
@@ -25,11 +25,11 @@ public class Follower extends Entity {
    */
   public Follower(User owner, User follower) {
     this.ownerId = owner.getId();
-    this.followerId = follower.getId();
-    this.followerFirstName = follower.getFirstName();
-    this.followerLastName = follower.getLastName();
-    this.followerUsername = follower.getUsername();
-    this.followerImageUrl = follower.getImageUrl();
+    this.id = follower.getId();
+    this.firstName = follower.getFirstName();
+    this.lastName = follower.getLastName();
+    this.username = follower.getUsername();
+    this.imageUrl = follower.getImageUrl();
   }
 
 
