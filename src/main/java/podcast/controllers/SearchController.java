68725edf365ccaa157663ef6b4fee43a81d2d3c3
Jma353 <jma353@cornell.edu>
2017-03-11 20:31:12 -0500

    Untested episode search endpoint

--- src/main/java/podcast/controllers/SearchController.java
+++ src/main/java/podcast/controllers/SearchController.java
@@ -1,10 +1,19 @@
 package podcast.controllers;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import podcast.models.entities.Episode;
+import podcast.models.formats.Failure;
+import podcast.models.formats.Result;
+import podcast.models.formats.Success;
 import podcast.search.PodcastsSearch;
 import podcast.search.UsersSearch;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Search REST API controller
@@ -22,7 +31,19 @@ public class SearchController {
     this.usersSearch = usersSearch;
   }
 
+  /** Search episodes **/
+  @RequestMapping(method = RequestMethod.GET, value="/episodes/{query}")
+  public ResponseEntity<Result> searchEpisodes(HttpServletRequest request,
+                                               @PathVariable("query") String query) {
+    try {
+      List<Episode> results = podcastsSearch.searchEpisodes(query);
+      return ResponseEntity.status(200).body(new Success("episodes", results));
+    } catch (Exception e) {
+      return ResponseEntity.status(400).body(new Failure(e.getMessage()));
+    }
+  }
 
-  // TODO - endpoints
+
+
 
 }
