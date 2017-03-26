12721df7fd37e14ec9d4985b2e7e96f69c23f88e
Jma353 <jma353@cornell.edu>
2017-03-26 14:48:22 -0400

    Start stubbing endpoints

--- src/main/java/podcast/controllers/PodcastsController.java
+++ src/main/java/podcast/controllers/PodcastsController.java
@@ -1,7 +1,13 @@
 package podcast.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import podcast.models.entities.User;
+import podcast.models.formats.Failure;
+import podcast.models.formats.Result;
+import podcast.models.formats.Success;
+import javax.servlet.http.HttpServletRequest;
+import static podcast.utils.Constants.*;
 
 /**
  * Podcasts (series, episodes) REST API controller
@@ -10,6 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/podcasts")
 public class PodcastsController {
 
-  // TODO - subscribing to series, recommending episodes, etc.
+  /** Get episodes by seriesId **/
+  @RequestMapping(method = RequestMethod.GET, value = "/{series_id}")
+  public ResponseEntity<Result> getEpisodesBySeriesId(HttpServletRequest request,
+                                                      @PathVariable("series_id") String seriesId,
+                                                      @RequestParam("offset") Integer offset,
+                                                      @RequestParam("max") Integer max) {
+    /* Grab the user from the corresponding request */
+    User user = (User) request.getAttribute(USER);
+    try {
+      // TODO - get episodes, sorted by release date
+      return ResponseEntity.status(200).body(new Success());
+    } catch (Exception e) {
+      return ResponseEntity.status(400).body(new Failure(e.getMessage()));
+    }
+  }
 
 }
