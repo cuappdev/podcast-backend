2cf0d8ecc27c5b9224b4853dfb17a953e350f0b3
Amit Mizrahi <amit@amizrahi.com>
2017-03-27 18:33:54 -0400

    Implement service methods for creating/deleting subscriptions

---
+++ src/main/java/podcast/services/SubscriptionsService.java
@@ -0,0 +1,39 @@
+package podcast.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import podcast.models.entities.Series;
+import podcast.models.entities.Subscription;
+import podcast.models.entities.User;
+import podcast.repos.PodcastsRepo;
+import podcast.repos.SubscriptionsRepo;
+import podcast.repos.UsersRepo;
+
+@Service
+public class SubscriptionsService {
+
+  private PodcastsRepo podcastsRepo;
+  private UsersRepo usersRepo;
+  private SubscriptionsRepo subscriptionsRepo;
+
+  @Autowired
+  public SubscriptionsService(PodcastsRepo podcastsRepo, SubscriptionsRepo subscriptionsRepo, UsersRepo usersRepo) {
+    this.podcastsRepo = podcastsRepo;
+    this.subscriptionsRepo = subscriptionsRepo;
+  }
+
+  public Subscription createSubscription(User owner, Series series) {
+    synchronized (this) {
+      Subscription sub = new Subscription(owner, series);
+      subscriptionsRepo.storeSubscription(sub);
+      return sub;
+    }
+  }
+
+  public boolean deleteSubscription(Subscription subscription) {
+    synchronized (this) {
+      return subscriptionsRepo.deleteSubscription(subscription);
+    }
+  }
+
+}
