dec153419914fe1c5fd03f0c4f3273d2d27ac3a1
Jma353 <jma353@cornell.edu>
2017-04-01 10:39:27 -0400

    Remove extra userId from subscriptions and update docs

--- src/main/java/podcast/models/entities/Subscription.java
+++ src/main/java/podcast/models/entities/Subscription.java
@@ -18,7 +18,6 @@ public class Subscription extends Entity {
   @Getter private String imageUrlLg;
   @Getter private AssociatedUser user;
   @Getter private Date createdAt = new Date();
-  @Getter private String userId;
 
 
   /**
@@ -32,7 +31,6 @@ public class Subscription extends Entity {
     this.imageUrlSm = series.getImageUrlSm();
     this.imageUrlLg = series.getImageUrlLg();
     this.user = new AssociatedUser(user);
-    this.userId = this.user.getId();
   }
 
 
@@ -43,7 +41,6 @@ public class Subscription extends Entity {
     this.imageUrlSm = object.getString(IMAGE_URL_SM);
     this.imageUrlLg = object.getString(IMAGE_URL_LG);
     this.user = new AssociatedUser(object.getObject(USER));
-    this.userId = this.user.getId();
   }
 
 
@@ -58,7 +55,7 @@ public class Subscription extends Entity {
 
   /** Compose Key from Subscription **/
   public static String composeKey(Subscription s) {
-    return composeKey(s.getUserId(), s.getSeriesId());
+    return composeKey(s.getUser().getId(), s.getSeriesId());
   }
 
 }
