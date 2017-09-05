3b1d7fa0a1189ee686e241c972bed8b71bd90ed2
Jesse Chen <jmc622@cornell.edu>
2017-09-04 22:07:00 -0400

    Implement followings model, dao, controller

---
+++ py/src/app/pcasts/controllers/get_user_followings_controller.py
@@ -0,0 +1,16 @@
+from . import *
+
+class GetUserFollowingsController(AppDevController):
+
+  def get_path(self):
+    return '/followings/show/<user_id>'
+
+  def get_methods(self):
+    return ['GET']
+
+  @authorize
+  def content(self, **kwargs):
+    user_id = request.view_args['user_id']
+    followings = followings_dao.get_followings(user_id)
+
+    return {'followings': following_schema.dump(f) for f in followings}
