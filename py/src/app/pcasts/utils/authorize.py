bcfc5cdbb0ba10d2ab44e550feb762c61feb3641
Jesse Chen <jmc622@cornell.edu>
2017-09-17 20:55:45 -0400

    Logic to mock authorize function for controller testing

--- py/src/app/pcasts/utils/authorize.py
+++ py/src/app/pcasts/utils/authorize.py
@@ -1,11 +1,21 @@
 import datetime
+import os
 from functools import wraps
-from flask import request, jsonify
+from app import constants
 from app.pcasts.dao import users_dao
+from flask import request, jsonify
 
 def authorize(f):
   @wraps(f)
   def authorization_decorator(*args, **kwargs):
+    if os.environ['APP_SETTINGS'] == constants.TEST_APP_SETTING:
+      test_user = users_dao.\
+        get_user_by_google_id(constants.TEST_USER_GOOGLE_ID1)
+      if test_user:
+        return f(user=test_user, *args, **kwargs)
+      else:
+        raise Exception('Test user not found')
+
     auth_header = request.headers.get('Authorization')
     if auth_header is None:
       raise Exception('Authorization header is not included!')
--- py/tests/loading_utils.py
+++ py/tests/loading_utils.py
@@ -5,13 +5,14 @@ src_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/src'
 sys.path.append(src_path)
 
 from app import app # pylint: disable=C0413
+from app import constants
 from app.pcasts.models._all import * # pylint: disable=C0413
 from app.pcasts.utils.db_utils import * # pylint: disable=C0413
 
 def load_users():
   default_users = [
       User(
-          google_id='default_google_id1',
+          google_id=constants.TEST_USER_GOOGLE_ID1,
           email='default_email1',
           first_name='default_first_name1',
           last_name='default_last_name1',
@@ -20,7 +21,7 @@ def load_users():
           followings_count=0,
       ),
       User(
-          google_id='default_google_id2',
+          google_id=constants.TEST_USER_GOOGLE_ID2,
           email='default_email2',
           first_name='default_first_name2',
           last_name='default_last_name2',
