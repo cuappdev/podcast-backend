b08ae9f738e70db942550330c6258e5fa0270f6e
Jma353 <jma353@cornell.edu>
2017-09-25 13:46:49 -0400

    Reorganize files but some tests failing

--- mapreduce/followings/map.js
+++
@@ -1,5 +0,0 @@
-function(doc, meta) {
-  if(doc.type == "FOLLOWING") {
-    emit(doc.id1, [doc.id2]);
-  }
-}
--- src/test/java/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootConfiguration
-@ComponentScan("podcast")
-public class AppConfig {
-}
