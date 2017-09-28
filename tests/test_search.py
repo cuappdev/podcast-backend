3d9b5612486c34604e09d0c7a6dd8b3e85ce03d2
DanielNosrati <dn259@cornell.edu>
2017-09-28 16:58:36 -0400

    Search series tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -35,7 +35,6 @@ class SearchTestCase(TestCase):
     ten_result_data = json.loads(search_results.data)
     self.assertEquals(4, len(ten_result_data['data']['episodes']))
 
-
     ##Test offset
     ten_result_title = 'big d'
     search_results = self.app.get('api/v1/search/episodes/{}?offset={}&max={}'\
@@ -44,8 +43,40 @@ class SearchTestCase(TestCase):
     self.assertEquals("Big Dare", \
         ten_result_data['data']['episodes'][0]['title'])
 
-  def search_series(self):
-    pass
+  def test_search_series(self):
+    no_result_title = 'ABCDEFGHIJKL'
+    search_results = self.app.get('api/v1/search/series/{}?offset={}&max={}'\
+         .format(no_result_title, 0, 1000))
+    no_result_data = json.loads(search_results.data)
+    self.assertEquals(0, len(no_result_data['data']['episodes']))
+
+    one_result_title = 'Jud'
+    search_results = self.app.get('api/v1/search/series/{}?offset={}&max={}'\
+         .format(one_result_title, 0, 1000))
+    one_result_data = json.loads(search_results.data)
+    self.assertEquals(1, len(one_result_data['data']['episodes']))
+
+    many_result_title = 'a'
+    search_results = self.app.get('api/v1/search/series/{}?offset={}&max={}'\
+         .format(many_result_title, 0, 1000))
+    many_result_data = json.loads(search_results.data)
+    self.assertEquals(32, len(many_result_data['data']['episodes']))
+
+    ##Test limit
+    ten_result_title = 'a'
+    search_results = self.app.get('api/v1/search/series/{}?offset={}&max={}'\
+         .format(ten_result_title, 0, 4))
+    ten_result_data = json.loads(search_results.data)
+    self.assertEquals(4, len(ten_result_data['data']['episodes']))
+
+    ##Test offset
+    ten_result_title = 'Cl'
+    search_results = self.app.get('api/v1/search/series/{}?offset={}&max={}'\
+         .format(ten_result_title, 2, 10))
+    ten_result_data = json.loads(search_results.data)
+    self.assertEquals("Clever", \
+        ten_result_data['data']['episodes'][0]['title'])
+
 
   def search_user(self):
     pass
