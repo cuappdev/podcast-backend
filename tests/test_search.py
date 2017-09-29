15e40d415424a52d930fe9b5a679c6ccaa0061b3
DanielNosrati <dn259@cornell.edu>
2017-09-28 20:41:29 -0400

    Search user tests done

--- tests/test_search.py
+++ tests/test_search.py
@@ -78,5 +78,36 @@ class SearchTestCase(TestCase):
         ten_result_data['data']['series'][0]['title'])
 
 
-  def search_user(self):
-    pass
+  def test_search_user(self):
+    no_result_username = 'ABCDEFGHIJKL'
+    search_results = self.app.get('api/v1/search/users/{}?offset={}&max={}'\
+         .format(no_result_username, 0, 1000))
+    no_result_data = json.loads(search_results.data)
+    self.assertEquals(0, len(no_result_data['data']['users']))
+
+    one_result_username = 'temp-default_google_id1'
+    search_results = self.app.get('api/v1/search/users/{}?offset={}&max={}'\
+         .format(one_result_username, 0, 1000))
+    one_result_data = json.loads(search_results.data)
+    self.assertEquals(1, len(one_result_data['data']['users']))
+
+    many_result_username = 'temp-default_google_id'
+    search_results = self.app.get('api/v1/search/users/{}?offset={}&max={}'\
+         .format(many_result_username, 0, 1000))
+    many_result_data = json.loads(search_results.data)
+    self.assertEquals(2, len(many_result_data['data']['users']))
+
+    ##Test limit
+    two_result_username = 'temp-default_google_id'
+    search_results = self.app.get('api/v1/search/users/{}?offset={}&max={}'\
+         .format(two_result_username, 0, 1))
+    ten_result_data = json.loads(search_results.data)
+    self.assertEquals(1, len(ten_result_data['data']['users']))
+
+    ##Test offset
+    two_result_username = 'temp-default_google_id'
+    search_results = self.app.get('api/v1/search/users/{}?offset={}&max={}'\
+         .format(two_result_username, 1, 10))
+    ten_result_data = json.loads(search_results.data)
+    self.assertEquals("temp-default_google_id2", \
+        ten_result_data['data']['users'][0]['username'])
