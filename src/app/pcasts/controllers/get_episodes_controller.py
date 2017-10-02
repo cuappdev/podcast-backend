67d543b05b92ccbe6a303340ccb3944d36cfac89
DanielNosrati <dn259@cornell.edu>
2017-10-02 01:54:16 -0400

    Search episodes by series + tests

---
+++ src/app/pcasts/controllers/get_episodes_controller.py
@@ -0,0 +1,19 @@
+from . import *
+
+class GetEpisodesController(AppDevController):
+
+  def get_path(self):
+    return '/podcasts/episodes/by_series/<series_id>/'
+
+  def get_methods(self):
+    return ['GET']
+
+  @authorize
+  def content(self, **kwargs):
+    series_id = request.view_args['series_id']
+    offset = request.args['offset']
+    max_search = request.args['max']
+    possible_episodes = episodes_dao.\
+        get_episodes_by_series(series_id, offset, max_search)
+    return {'episodes': \
+        [episode_schema.dump(e).data for e in possible_episodes]}
