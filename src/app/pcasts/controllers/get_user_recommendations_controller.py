a37253edec1836c70989a120108c879a95b641a4
Jesse Chen <jmc622@cornell.edu>
2017-10-15 14:49:56 -0400

    Fix get_user_recommendations bug where episode context was wrong user

--- src/app/pcasts/controllers/get_user_recommendations_controller.py
+++ src/app/pcasts/controllers/get_user_recommendations_controller.py
@@ -10,8 +10,12 @@ class GetUserRecommendationsController(AppDevController):
 
   @authorize
   def content(self, **kwargs):
-    user_id = request.view_args['user_id']
-    recommendations = recommendations_dao.get_user_recommendations(user_id)
+    caller_user_id = kwargs.get('user').id
+    requested_user_id = request.view_args['user_id']
+    recommendations = recommendations_dao.get_user_recommendations(
+        caller_user_id,
+        requested_user_id
+    )
 
     return {'recommendations': \
       [recommendation_schema.dump(r).data for r in recommendations]}
