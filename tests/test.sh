23d21dcdd900c11cd0721f613d7363792201f4fb
Jma353 <jma353@cornell.edu>
2017-11-01 15:51:04 -0400

    iTunes smoke test + error handling on improper data

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -eq 0 ]
 then
-  python ../venv/bin/nosetests --nocapture
+  python ../venv/bin/nosetests --nocapture -s
 else
-  python ../venv/bin/nosetests $1 --nocapture
+  python ../venv/bin/nosetests $1 --nocapture -s
 fi
