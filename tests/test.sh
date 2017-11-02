bac1d665fa722bccc6c0062d53c84df0b3dc9f2c
Jma353 <jma353@cornell.edu>
2017-11-01 20:14:47 -0400

    Remove relic dash s

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -eq 0 ]
 then
-  python ../venv/bin/nosetests --nocapture -s
+  python ../venv/bin/nosetests --nocapture
 else
-  python ../venv/bin/nosetests $1 --nocapture -s
+  python ../venv/bin/nosetests $1 --nocapture
 fi
