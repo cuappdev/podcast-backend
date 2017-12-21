177ed902535858a2a916deda45969a2499a82bf6
DanielNosrati <dn259@cornell.edu>
2017-12-21 17:50:30 -0500

    Added FB profile pictures

--- src/app/pcasts/utils/facebook_utils.py
+++ src/app/pcasts/utils/facebook_utils.py
@@ -2,7 +2,7 @@ import requests
 
 def get_me(access_token):
   base_uri = "https://graph.facebook.com/v2.10/me" \
-      +"?fields=id%2Cname%2Cfirst_name%2Clast_name&access_token="
+      +"?fields=id%2Cname%2Cfirst_name%2Clast_name%2Cpicture&access_token="
   uri = '{}{}'.format(base_uri, access_token)
   return requests.get(uri).json()
 
