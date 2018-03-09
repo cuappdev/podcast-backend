43f93f90e81f9b02ee874057525792999dc6566f
DanielNosrati <dn259@cornell.edu>
2018-03-08 20:06:14 -0500

    Added extra fields to episode by series return json

--- src/app/pcasts/controllers/get_episodes_controller.py
+++ src/app/pcasts/controllers/get_episodes_controller.py
@@ -10,10 +10,11 @@ class GetEpisodesController(AppDevController):
 
   @authorize
   def content(self, **kwargs):
+    user = kwargs.get('user')
     series_id = request.view_args['series_id']
     offset = request.args['offset']
     max_search = request.args['max']
     possible_episodes = episodes_dao.\
-        get_episodes_by_series(series_id, offset, max_search)
+        get_episodes_by_series(series_id, offset, max_search, user.id)
     return {'episodes': \
         [episode_schema.dump(e).data for e in possible_episodes]}
