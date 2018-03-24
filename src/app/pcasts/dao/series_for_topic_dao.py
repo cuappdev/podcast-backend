0c90f8ca96f3d93c2040d7a2250c13bf3252e2af
kevingreer <kevingreer@me.com>
2018-03-24 14:32:04 -0400

    series for topic

---
+++ src/app/pcasts/dao/series_for_topic_dao.py
@@ -0,0 +1,9 @@
+from . import *
+
+def get_series_list_for_topic(tid):
+  optional_model = SeriesForTopic.query \
+    .filter(SeriesForTopic.topic_id == tid).first()
+  if optional_model:
+    return [int(sid) for sid in optional_model.series_list.split(',')]
+  else:
+    raise Exception('No topic id {} exists'.format(tid))
