986298633bf06a7b9174995605fa8fcc00e51275
kevingreer <kevingreer@me.com>
2018-03-27 21:31:14 -0400

    tests and overall top series for topic

--- src/app/pcasts/dao/series_for_topic_dao.py
+++ src/app/pcasts/dao/series_for_topic_dao.py
@@ -1,6 +1,7 @@
 from . import *
 
 def get_series_list_for_topic(tid):
+  tid = 1 if tid == 'all' else int(tid)
   optional_model = SeriesForTopic.query \
     .filter(SeriesForTopic.topic_id == tid).first()
   if optional_model:
